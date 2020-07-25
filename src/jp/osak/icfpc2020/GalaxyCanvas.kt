16af25c3678e478024e6b01480a4ae19759b508f
Osamu Koga <osak.63@gmail.com>
2020-07-25 03:27:11 -0500

    Move run into GalaxyCanvas

--- src/jp/osak/icfpc2020/GalaxyCanvas.kt
+++ src/jp/osak/icfpc2020/GalaxyCanvas.kt
@@ -6,9 +6,10 @@ import java.awt.Graphics
 
 data class Vec(val x: Int, val y: Int)
 
-class GalaxyCanvas : Canvas() {
-    var data: List<List<Vec>> = listOf()
-    val colors = listOf(Color.BLUE, Color.GREEN, Color.RED, Color.GRAY, Color.ORANGE)
+class GalaxyCanvas(private val engine: GalaxyEngine) : Canvas() {
+    private var state: Term = Lambda(Lambda.Type.NIL)
+    private var data: List<List<Vec>> = listOf()
+    private val colors = listOf(Color.BLUE, Color.GREEN, Color.RED, Color.GRAY, Color.ORANGE)
 
     override fun paint(g: Graphics) {
         g.clearRect(0, 0, width, height)
@@ -16,13 +17,44 @@ class GalaxyCanvas : Canvas() {
         for (image in data) {
             g.color = colors[i++]
             for (p in image) {
-                g.fillRect(p.x * 3 + width/2, p.y * 3 + height/2, 3, 3)
+                g.fillRect(p.x * 3 + width / 2, p.y * 3 + height / 2, 3, 3)
             }
         }
     }
 
-    fun draw(data: List<List<Vec>>) {
-        this.data = data
+    fun run(p: Vec) {
+        val program =
+            App(
+                App(
+                    Lambda(Lambda.Type.REF, listOf(Name("galaxy"))),
+                    state
+                ),
+                Lambda(Lambda.Type.CONS, listOf(Num(p.x.toLong()), Num(p.y.toLong())))
+            )
+        val output = engine.evaluateFull(program)
+        state = engine.car(engine.cdr(output))
+        val imagesConsList = engine.car(engine.cdr(engine.cdr(output)))
+        data = imagesConsList.asSequence().map { toVecList(it) }.toList()
         repaint()
+    }
+
+    fun toVecList(t: Term): List<Vec> {
+        return t.asSequence().map {
+            val x = (engine.car(it) as Num).v
+            val y = (engine.cdr(it) as Num).v
+            Vec(x.toInt(), y.toInt())
+        }.toList()
+    }
+
+    fun Term.isNil() = this is Lambda && this.type == Lambda.Type.NIL
+
+    fun Term.asSequence(): Sequence<Term> {
+        var cur = this
+        return sequence {
+            while (!cur.isNil()) {
+                yield(engine.car(cur))
+                cur = engine.cdr(cur)
+            }
+        }
     }
 }
--- src/jp/osak/icfpc2020/Main.kt
+++ src/jp/osak/icfpc2020/Main.kt
@@ -17,18 +17,14 @@ class Main {
         val frame = JFrame("Galaxy")
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
-        val canvas = GalaxyCanvas()
+        val canvas = GalaxyCanvas(engine)
         frame.contentPane.add(canvas)
         frame.setSize(600, 600)
         frame.isVisible = true
 
         canvas.createBufferStrategy(2)
 
-        val eval = parser.parseGalaxy("ap ap :galaxy nil ap ap cons 0 0")
-        val output = engine.evaluateFull(eval)
-        val imagesConsList = engine.car(engine.cdr(engine.cdr(output)))
-        val images = imagesConsList.asSequence().map { toVecList(it) }.toList()
-        canvas.draw(images)
+        canvas.run(Vec(0, 0))
     }
 
     fun loadPrelude(): Map<String, Term> {
@@ -39,50 +35,6 @@ class Main {
                 Pair(name, parser.parseGalaxy(values[1].trim()))
             }.toMap()
         }
-    }
-
-    fun dumpConsList(t: Term) {
-        val res = engine.evaluateFull(t)
-        if (res is Num) {
-            print(res.v)
-        } else {
-            require (res is Lambda)
-            if (res.type == Lambda.Type.NIL) {
-                print("nil")
-            } else {
-                print("(")
-                dumpConsList(engine.car(res))
-                print(",")
-                dumpConsList(engine.cdr(res))
-                print(")")
-            }
-        }
-    }
-
-    fun toVecList(t: Term): List<Vec> {
-        return t.asSequence().map {
-            val x = (engine.car(it) as Num).v
-            val y = (engine.cdr(it) as Num).v
-            Vec(x.toInt(), y.toInt())
-        }.toList()
-    }
-
-    fun Term.isNil() = this is Lambda && this.type == Lambda.Type.NIL
-
-    fun Term.asSequence(): Sequence<Term> {
-        var cur = this
-        return sequence {
-            while (!cur.isNil()) {
-                yield(engine.car(cur))
-                cur = engine.cdr(cur)
-            }
-        }
-    }
-
-    fun test() {
-        val eval = parser.parseGalaxy("ap ap :galaxy nil ap ap cons 0 0")
-        val result = engine.evaluateFull(eval)
-        dumpConsList(result)
     }
 
     companion object {
